6b1ac1e3aefc09c22b210170ccc86aa038305ca5
Abir Tasrif Anto <abirtasrif@gmail.com>
2023-03-08 23:07:44 +0600

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="app container mx-auto">
+      <div className="upper-content min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Chat />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
